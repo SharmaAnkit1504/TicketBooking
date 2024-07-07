721afb3b0250452313747692bfab48b365ae2d9e
SharmaAnkit1504 <sharma.ankit9826@outlook.com>
2024-07-07 12:29:21 +0530

    codepshedfromlocal

---
+++ src/main/java/com/test/TicketBooking/controller/BusTicketController.java
@@ -0,0 +1,21 @@
+package com.test.TicketBooking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BusTicketController {
+
+
+
+
+    @GetMapping("/busbooking")
+    public String createBooking(){
+
+        double pnr = Math.random();
+
+        return "bus booked successfully with PNR number :: "+pnr;
+    }
+
+
+}
